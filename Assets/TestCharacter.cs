50bca27e833ccba738b59d439a5f6be2da117483
Morris Arroyo <arroyomorris28@gmail.com>
2018-01-27 00:34:59 -0800

    working mirror test

---
+++ Assets/TestCharacter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacter : MonoBehaviour
+{
+
+    public float speed = 6f;
+    public GameObject mirrorObject;
+
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    Mirror mirror;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        mirror = mirrorObject.GetComponent<Mirror>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            mirror.RotateLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            mirror.RotateRight();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(-h, -v);
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mirror.Rotate(playerRigidbody.position);
+        }
+        */
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
---
+++ Assets/TestLight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLight : MonoBehaviour
+{
+    Ray shootRay = new Ray();
+    RaycastHit shootHit;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+        if (Physics.Raycast(shootRay, out shootHit))
+        {
+            Mirror mirror = shootHit.collider.GetComponent<Mirror>();
+            //Debug.Log("hello", shootHit.collider);
+            mirror.Reflect(transform.position, shootHit);
+        }
+    }
+}
