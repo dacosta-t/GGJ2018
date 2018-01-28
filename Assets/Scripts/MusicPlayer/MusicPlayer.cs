550234155b321b7d1a6c23109389f381d6ecf07a
Tyler Da Costa <dacosta-t@hotmail.com>
2018-01-28 13:30:56 -0800

    Music player continues playing through levels

---
+++ Assets/Scripts/MusicPlayer/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    static bool loaded = false;
+    // Use this for initialization
+    void Awake() {
+        if (!loaded) {
+            loaded = true;
+            GetComponent<AudioSource>().Play();
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+}
